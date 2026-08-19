3f5eee66f727179e1e412e8089eb30ab42426cbf
Lifx Mode Safe <alif.diaz257@sma.belajar.id>
2026-08-19 20:05:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,14 @@ module.exports = {
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backdropBlur: {
+        xs: '2px',
+        sm: '4px',
+        md: '8px',
+        lg: '12px',
+        xl: '16px',
       },
     },
   },
